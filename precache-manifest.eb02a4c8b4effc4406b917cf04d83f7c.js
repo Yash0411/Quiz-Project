a6852b9cef95a187e5f6f783d7ea32a8e3f763ad
Yash0411 <ynm0411@gmail.com>
2020-08-25 22:36:38 +0530

    Updates

---
+++ precache-manifest.eb02a4c8b4effc4406b917cf04d83f7c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf2648f04a6b966d66945214f1226c8",
+    "url": "/Quiz-Project/index.html"
+  },
+  {
+    "revision": "d694ad1b9aaf469390fe",
+    "url": "/Quiz-Project/static/css/main.58a6b47b.chunk.css"
+  },
+  {
+    "revision": "e157263aaee979cdec81",
+    "url": "/Quiz-Project/static/js/2.25d1a7a4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Quiz-Project/static/js/2.25d1a7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d694ad1b9aaf469390fe",
+    "url": "/Quiz-Project/static/js/main.1aa7950c.chunk.js"
+  },
+  {
+    "revision": "a271251d9e4bf13c7239",
+    "url": "/Quiz-Project/static/js/runtime-main.40c3df00.js"
+  },
+  {
+    "revision": "9f5b8c0e5b54d6d7f3cb62b78b8ba128",
+    "url": "/Quiz-Project/static/media/Blog.9f5b8c0e.PNG"
+  },
+  {
+    "revision": "a0357a84543c93184452121b0976285f",
+    "url": "/Quiz-Project/static/media/Video.a0357a84.PNG"
+  },
+  {
+    "revision": "1aeb9e710b10d364bfb8cd71bcc73692",
+    "url": "/Quiz-Project/static/media/analysisbg2.1aeb9e71.PNG"
+  },
+  {
+    "revision": "8829697b49d484ed361287132c3ddfbe",
+    "url": "/Quiz-Project/static/media/analyticsboardimg.8829697b.PNG"
+  },
+  {
+    "revision": "97941b01eef87b80795720da4f78f0a9",
+    "url": "/Quiz-Project/static/media/assesmenthead.97941b01.png"
+  },
+  {
+    "revision": "b6c9f459221ac91a71c955b34fa392d1",
+    "url": "/Quiz-Project/static/media/calculator.b6c9f459.PNG"
+  },
+  {
+    "revision": "1638e857f25c56c1982a3fd6ef7f3d7b",
+    "url": "/Quiz-Project/static/media/desktop.1638e857.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Quiz-Project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4dd12fc33038e9e3663eb1c799741759",
+    "url": "/Quiz-Project/static/media/pc.4dd12fc3.PNG"
+  },
+  {
+    "revision": "a82133a16e78bae323fd01974de658e9",
+    "url": "/Quiz-Project/static/media/portfolio-1.a82133a1.jpg"
+  },
+  {
+    "revision": "df34785894efaa3b27a71b8fc87d71f3",
+    "url": "/Quiz-Project/static/media/user.df347858.PNG"
+  }
+]);
